59f1e1ff451744edcad6a643b259a7ce70a823bd
gilotin <toshevnikolay@gmail.com>
2025-02-25 01:30:18 +0200

    FIX: Minor Changes

--- ai.js
+++ ai.js
@@ -1,6 +1,4 @@
-import Anthropic from "@anthropic-ai/sdk";
 import { HfInference } from "@huggingface/inference";
-const token = import.meta.env.VITE_HF_ACCESS_TOKEN;
 
 const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`;
 
@@ -17,7 +15,9 @@ const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients 
 
 // Make sure you set an environment variable in Scrimba
 // for HF_ACCESS_TOKEN
-const hf = new HfInference(import.meta.env.VITE_HF_ACCESS_TOKEN);
+
+const token = import.meta.env.VITE_HF_ACCESS_TOKEN;
+const hf = new HfInference(token);
 
 export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ");
