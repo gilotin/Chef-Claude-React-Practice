56f6afb0d1acbb7b8cecce91b0188750bad9902c
gilotin <toshevnikolay@gmail.com>
2025-02-25 23:36:58 +0200

    FEAT: Added some styling

--- components/ClaudeRecipe.jsx
+++ components/ClaudeRecipe.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import ReactMarkdown from "react-markdown";
 
 export default function ClaudeRecipe(props) {
     const markdown = `${props.recipe}`;
 
     return (
-        <div>
+        <section className="suggested-recipe-container" aria-live="polite">
+            <h2>Chef Claude Recommends:</h2>
             <ReactMarkdown children={markdown} />
-        </div>
+        </section>
     );
 }
